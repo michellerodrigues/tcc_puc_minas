5300de1ce2b9e12ba8084aa5342a1bc18fd160a1
Michelle Rodrigues <mica.msr@gmail.com>
2019-03-16 16:47:18 -0300

    última cofniguração nservice busc, válida. Finalmente!!!

--- AgendaService/Startup.cs
+++ AgendaService/Startup.cs
@@ -17,6 +17,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Messages.Descartes.Commands;
 using AgendaService.Saga;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using Newtonsoft.Json;
 
 namespace AgendaService
 {
@@ -64,44 +67,68 @@ namespace AgendaService
 
         public void ConfigureNserviceBus(IServiceCollection services, AppDataContext context)
         {
-                        /* 
-            var endpointName = "Descarte.Messages";
-            var machineName = $"{Dns.GetHostName()}.{IPGlobalProperties.GetIPGlobalProperties().DomainName}";
-            var instanceIdentifier = $"{endpointName}@{machineName}";*/
-            var endpointConfiguration = new EndpointConfiguration("Descarte.Agendamento");
+            
+            var endpointName = "Descarte.Agenda";
+            var conexaoQueue = "host=localhost;user=guest;password=guest";
+            var sendFailedMessagesTo = "Particular.ServiceControl.Error";
+           var auditProcessedMessagesTo = "Particular.ServiceControl.Audit";
+            var serviceControlQueueName = "Particular.ServiceControl";
+            const string SERVICE_CONTROL_METRICS_ADDRESS = "Particular.Monitoring";
 
+
+            var machineName = $"{Dns.GetHostName()}.{IPGlobalProperties.GetIPGlobalProperties().DomainName}";
+            var instanceIdentifier = $"{endpointName}@{machineName}";
+            
+            var endpointConfiguration = new EndpointConfiguration(endpointName);
 
             var transport = endpointConfiguration.UseTransport<RabbitMQTransport>()
-            //passar isso para o webconfig  
-            .ConnectionString("host=localhost;user=guest;password=guest")
+            .ConnectionString(conexaoQueue)
             .UseConventionalRoutingTopology();
             
-            //
-            const string SERVICE_CONTROL_METRICS_ADDRESS = "Descarte.Monitoring";
+            endpointConfiguration.SendFailedMessagesTo(sendFailedMessagesTo);
+            endpointConfiguration.AuditProcessedMessagesTo(auditProcessedMessagesTo);
 
-            var hostId = new Guid();
+            var hostId = Guid.NewGuid();
             endpointConfiguration.UniquelyIdentifyRunningInstance()
                 .UsingCustomIdentifier(hostId);
+            
 
+            
+            
+            //metric não serve pra sendonly
             var metrics = endpointConfiguration.EnableMetrics();
 
             metrics.SendMetricDataToServiceControl(
                 serviceControlMetricsAddress: SERVICE_CONTROL_METRICS_ADDRESS,
                 interval: TimeSpan.FromSeconds(10),
-                instanceId: "Descarte.Agendamento");
+                instanceId: endpointName);
 
-            endpointConfiguration.SendFailedMessagesTo("Descarte.Error");
-            endpointConfiguration.AuditProcessedMessagesTo("Descarte.Audit");
+
+            endpointConfiguration.ReportCustomChecksTo(
+                serviceControlQueue: serviceControlQueueName);
+
+            endpointConfiguration.SendHeartbeatTo(
+                serviceControlQueue: serviceControlQueueName,
+                frequency: TimeSpan.FromSeconds(15));
+
+
+            endpointConfiguration.AuditSagaStateChanges(
+                serviceControlQueue: serviceControlQueueName);
+
             endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
-            endpointConfiguration.AuditSagaStateChanges(serviceControlQueue: "DescarteServiceMonitoring");
 
             var persistence = endpointConfiguration.UsePersistence<NHibernatePersistence>();
             persistence.ConnectionString(@"Server = DESKTOP-C8BIS20\MSSQLSERVER2;Database=SagaDescarteDB;Integrated Security=True;"); //@ na frente
-                
-            endpointConfiguration.EnableInstallers();
+            
+         //   endpointConfiguration.SendOnly();  
 
-           // var routing = transport.Routing();
-          //  routing.RouteToEndpoint(typeof(PlaceOrder), "Sales");
+
+            var routing = transport.Routing();
+            routing.RouteToEndpoint(typeof(AgendarRetiradaCommand), endpointName);
+            routing.RouteToEndpoint(typeof(ConfirmarAgendamentoCommand), endpointName);
+            routing.RouteToEndpoint(typeof(CancelarAgendamentoRetiradaCommand), endpointName);
+            routing.RouteToEndpoint(typeof(CancelarAgendamentoConfirmadoRetiradaCommand), endpointName);
+
             IEndpointInstance endpoint = null;
 
            
@@ -111,15 +138,7 @@ namespace AgendaService
                 customizations.ExistingServices(services);
             });
            
-            //endpointConfiguration.SendOnly();  
-
-            /* 
-            var routing = transport.Routing();
-            routing.RouteToEndpoint(
-                assembly: typeof(AgendarRetiradaCommand).Assembly,
-                destination: "Descarte.Agendamento");*/
-
-
+            endpointConfiguration.EnableInstallers();
             endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();   
                          
             services.AddSingleton<IMessageSession>(endpoint);
